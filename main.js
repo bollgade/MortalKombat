c86a8e0435bb1a95a687ee7c65885eaec70f3d8d
Bollgade <Bollgade@yandex.ru>
2021-10-14 19:27:50 +0300

    CompleteAllTasks

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const player1 = {
   name: 'Sub-Zero',
   hp: 89,
-  img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+  img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
   weapon: ['Katana', 'Kunai'],
   attack: function() {
     console.log(player1.name + ' fight');
@@ -17,3 +17,38 @@ const player2 = {
     console.log(player1.name + ' fight');
   },
 }
+
+const $arena = document.querySelector('.arenas');
+
+function createPlayer(character, num) {
+  const $player = document.createElement('div');
+  $player.classList.add('player' + num);
+  
+  const $progress = document.createElement('div');
+  $progress.classList.add('progressbar');
+  $player.appendChild($progress);
+
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+  $life.style.width = character.hp + '%';
+  $progress.appendChild($life);
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+  $name.innerText = character.name;
+  $progress.appendChild($name);
+
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+  $player.appendChild($character);
+
+  const $img = document.createElement('img');
+  $img.classList.add('img');
+  $img.src = character.img;
+  $character.appendChild($img);
+
+  $arena.appendChild($player);
+}
+
+console.log(createPlayer(player1, 1));
+console.log(createPlayer(player2, 2));
