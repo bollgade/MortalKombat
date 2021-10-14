76f39b45da25afbfc230be572343194b39ea9dc4
Bollgade <Bollgade@yandex.ru>
2021-10-14 18:10:31 +0300

    CompleteTask#0

---
+++ main.js
@@ -0,0 +1,19 @@
+const player1 = {
+  name: 'Sub-Zero',
+  hp: 89,
+  img: 'https://www.fightersgeneration.com/characters3/subzero-mk2-fix.gif',
+  weapon: ['Katana', 'Kunai'],
+  attack: function() {
+    console.log(player1.name + ' fight');
+  },
+}
+
+const player2 = {
+  name: 'Scorpion',
+  hp: 65,
+  img: 'https://i.gifer.com/Ymo8.gif',
+  weapon: ['Katana', 'Kunai'],
+  attack: function() {
+    console.log(player1.name + ' fight');
+  },
+}
